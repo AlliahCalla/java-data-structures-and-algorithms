f4be59cc2fe0435287d4d675396ced0a245268ae
Alliah <alliahczarielle17@gmail.com>
2021-02-05 19:02:39 +1300

    Added String Reverser

--- src/com/alliah/Main.java
+++ src/com/alliah/Main.java
@@ -1,34 +1,13 @@
 package com.alliah;
 
-import com.alliah.arrays.Array;
-import com.alliah.linkedList.LinkedList;
-
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.Stack;
 
 public class Main {
-
     public static void main(String[] args) {
-		var list = new LinkedList();
-		list.addFirst(10);
-		list.addLast(20);
-		list.addLast(30);
-		list.addLast(40);
-		list.addLast(50);
-		list.addLast(60);
-		list.addFirst(0);
-		list.addLast(70);
+    	String str ="abcd";
+    	StringReverser reverser = new StringReverser();
+    	var reversed =reverser.reverse(str);
 
-		var array = list.toArray();
-		System.out.println(Arrays.toString(array));
-		System.out.println(list.getKthFromTheEnd(5));
-
-
-
-
-
-
-
-
+        System.out.println(reversed);
     }
 }
