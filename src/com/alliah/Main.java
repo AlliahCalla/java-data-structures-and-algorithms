6341bccc252a7a0f714ab452d6c55b49ffd37ec5
Alliah <alliahczarielle17@gmail.com>
2021-02-15 08:20:37 +1300

    Added Priority Queue that uses an Array

--- src/com/alliah/Main.java
+++ src/com/alliah/Main.java
@@ -7,11 +7,11 @@ public class Main {
         PriorityQueue queue = new PriorityQueue();
         // insert(2)
         //[1, 3, 3, 5 ,6]
-//        queue.add(3);
-//        queue.add(5);
-//        queue.add(1);
-//        queue.add(6);
-//        queue.add(2);
+        queue.add(3);
+        queue.add(5);
+        queue.add(1);
+        queue.add(6);
+        queue.add(2);
         System.out.println(queue.isFull());
         System.out.println(queue.isEmpty());
 
