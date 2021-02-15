2847dec30cbaf0631e52a8c711dce7c5c296672e
Alliah <alliahczarielle17@gmail.com>
2021-02-15 15:27:58 +1300

    Added CharFinder class

--- src/com/alliah/Main.java
+++ src/com/alliah/Main.java
@@ -6,26 +6,9 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) {
         String str = "a green apple";
-        Map<Character, Integer> map = new HashMap<>();
-
-        var chars = str.toCharArray();
-        for(char ch: chars){
-            if(map.containsKey(ch)){
-                int count = map.get(ch);
-                map.put(ch, count+1);
-            }else
-                map.put(ch, 1);
-        }
-        System.out.println(map);
-
-        for(var ch: chars)
-            if(map.get(ch) == 1){
-                System.out.println(ch);
-                break;
-            }
-
-
-        System.out.println(Character.MIN_VALUE);
+        CharFinder charFinder = new CharFinder();
+        var character = charFinder.findFirstNonRepeatingChar(str);
+        System.out.println(character);
 
 
     }
